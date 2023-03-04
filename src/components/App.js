632547c2091d02bcf2d6ada45468d9f176ead390
SYED RIYAZ <syedriyaz@192.168.1.10>
2023-03-04 22:57:06 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-//code here 
-  
+  //code here 
+  let [isLoggedIn, setLoggedIn] = useState(false);
 
-  
+  const handleClick = () => {
+    setLoggedIn(!isLoggedIn);
+
+  };
+
+
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+        {/* assign value for button 1 */}
+        {
+          isLoggedIn ? "ON" : "OFF"
+        }
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
         {/* assign value for button 2 */}
+        {
+          isLoggedIn ? "OFF" : "ON"
+        }
       </button>
     </div>
   );
